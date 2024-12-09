f70e1b24dd74aae82b21990ee3d35eca700bb6c2
koudimounika <152537922+koudimounika@users.noreply.github.com>
2024-12-09 14:38:46 -0500

    Update constants.ts

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,3 +1,3 @@
 export const Constants = {
-    BACKEND_API_URL: 'a7808bb7d67bc4534a44ff582265ed42-1585199303.us-east-1.elb.amazonaws.com',
+    BACKEND_API_URL: 'http://a7808bb7d67bc4534a44ff582265ed42-1585199303.us-east-1.elb.amazonaws.com',
 };
